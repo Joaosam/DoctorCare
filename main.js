1a024ffc834987548d0ea36c620700b7aa057f31
João Pedro Mendes dos Santos <66846607+Joaosam@users.noreply.github.com>
2022-05-07 22:41:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+/* Adiciono o evento scroll utilizando a função roll */
+addEventListener('scroll', roll)
+
+/*  Na função roll eu linko a função showNav e backToTop */
 function roll() {
+  showNav()
+  backToTop()
+}
+
+function showNav() {
   const navigation = document.getElementById('navigation')
   if (scrollY != 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function backToTop() {
+  const buttonTop = document.getElementById('backToTopButton')
+  if (scrollY > 495) {
+    buttonTop.classList.add('show')
+  } else {
+    buttonTop.classList.remove('show')
   }
 }
 
@@ -18,7 +36,7 @@ function closeMenu() {
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 750
+  duration: 700
 }).reveal(
-  `#home, .logoMulher, #home .stats, #services, #services header, .card, #about, #about header, #about .content, #about img`
+  `#home, .logoMulher, #home .stats, #services, #services header, .card, #about, #about header, #about .content, #about img, #contact, #contact header, #contact .content, #contact .button, #contact img`
 )
